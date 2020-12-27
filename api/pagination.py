943daa7ae34f9b664751258ea07efd651ef05880
Demon000 <demonsingur@gmail.com>
2020-12-27 07:15:58 +0200

    server: better pagination info

--- api/pagination.py
+++ api/pagination.py
@@ -11,6 +11,13 @@ def default_mapping_fn(item, *args, **kwargs):
     return item.to_dict()
 
 
+def get_page_count(no_items, limit) -> int:
+    if no_items:
+        return ceil(no_items / limit)
+    else:
+        return 0
+
+
 def get_paginated_items_from_qs(qs: QuerySet, mapping_fn=default_mapping_fn, *args, **kwargs):
     page = request.args.get('page', default=0)
     limit = request.args.get('limit', default=MAX_PAGINATED_LIMIT)
@@ -32,17 +39,19 @@ def get_paginated_items_from_qs(qs: QuerySet, mapping_fn=default_mapping_fn, *ar
 
     no_total_items = qs.count()
     no_items = qs.count(with_limit_and_skip=True)
-    if no_total_items:
-        no_pages = ceil(no_total_items / limit)
-    else:
-        no_pages = 0
+    no_items_before = max(skip, 0)
+    no_items_after = max(no_total_items - skip - no_items, 0)
 
     return {
         'limit': limit,
         'skip': skip,
-        'page': page,
         'no_items': no_items,
         'no_total_items': no_total_items,
-        'no_pages': no_pages,
+        'no_items_before': no_items_before,
+        'no_items_after': no_items_after,
+        'page': page,
+        'no_pages': get_page_count(no_total_items, limit),
+        'no_pages_before': get_page_count(no_items_before, limit),
+        'no_pages_after': get_page_count(no_items_after, limit),
         'items': [mapping_fn(item, *args, **kwargs) for item in qs],
     }
